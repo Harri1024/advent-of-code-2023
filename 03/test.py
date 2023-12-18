8c2d96821f78ec7f7340ec07ab8c9c226d79ef17
Harri Laaksonen <harri1024@gmail.com>
2023-12-18 22:28:49 +0200

    day3 testing

--- 03/test.py
+++ 03/test.py
@@ -1,15 +1,20 @@
 import re
 
 numbers_list = [(0, 12, 15), (0, 17, 20), (0, 27, 29), (2, 31, 34), (3, 31, 34)]
-symbols_list = [(2, 15, '#'), (0, 16, '#'), (1, 34, '&'), (1, 38), '#', (4, 34, '#')]
+symbols_list = [(2, 15, '#'), (0, 16, '*'), (1, 34, '&'), (1, 38, '#'), (4, 34, '*')]
 
 
 for number in numbers_list:
     for number_to_check in range(number[1] - 1, number[2] + 1):
         for offset in (-1, 0, 1):
-            if (number[0] + offset, number_to_check) in symbols_list:
-                print(number)
-                break 
+            key = (number[0] + offset, number_to_check)
+            for symbol in symbols_list:
+                x = (symbol[0], symbol[1])
+                if x == key:
+                    print(number)
+                    if symbol[2] == "*":
+                        print(symbol)
+                    break
 
 
 list = [(1, 2), (4, 5)]
--- 03/test2.py
+++ 03/test2.py
@@ -3,3 +3,11 @@ t2 = (1, 2, "asasa")
 
 if (t1[0], t1[1]) == (t2[0], t2[1]):
     print(t2[2])
+
+
+a = {}
+
+a['first'] += ['aaa']
+a['first'] += ['bbb']
+
+print(a)
